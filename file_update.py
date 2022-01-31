1a6860f29ddcc4e755ca1d9256414e7e60104fa2
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-31 15:23:01 +0530

    Add Watcher class to watch for file updates

--- file_update.py
+++ file_update.py
@@ -1,27 +1,39 @@
 import os
+import sys
+import time
 
 
-def file_is_updated(file_name):
+class Watcher(object):
     """
-    Checks if the given file is updated.
-    Returns True if updated, else False.
+    A class to check if a file has been updated.
     """
 
-    class FileUpdated(object):
-        """
-        A class to check if a file has been updated.
-        """
-        def __init__(self):
-            self._cached_stamp = 0
-            self.filename = file_name
+    running = True
+    refresh_delay_secs = 1
 
-        def check_file_update(self):
-            stamp = os.stat(self.filename).st_mtime
-            if stamp != self._cached_stamp:
-                self._cached_stamp = stamp
-                return True
-            else:
-                return False
+    # Constructor
+    def __init__(self, watch_file, call_func_on_change=None, *args, **kwargs):
+        self._cached_stamp = 0
+        self.filename = watch_file
+        self.call_func_on_change = call_func_on_change
+        self.args = args
+        self.kwargs = kwargs
 
-    file_updated = FileUpdated()
-    return file_updated.check_file_update()
+    def look(self):
+        stamp = os.stat(self.filename).st_mtime
+        if stamp != self._cached_stamp:
+            self._cached_stamp = stamp
+            if self.call_func_on_change is not None:
+                self.call_func_on_change(*self.args, **self.kwargs)
+
+    def watch(self):
+        while self.running:
+            try:
+                time.sleep(self.refresh_delay_secs)
+                self.look()
+            except KeyboardInterrupt:
+                sys.exit()
+            except FileNotFoundError:
+                pass
+            except:
+                print("Unhandled error: %s" % sys.exc_info()[0])
