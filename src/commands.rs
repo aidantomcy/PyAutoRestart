4e26c18d6ebbc797b8f1b30aadb35e8e3e56a199
Aidan Tomcy <aidantomcy@gmail.com>
2022-09-13 09:26:42 +0530

    Add semicolons

--- src/commands.rs
+++ src/commands.rs
@@ -2,13 +2,13 @@ use std::path::Path;
 
 pub fn help() {
     println!("Usage: pymon <file_name>");
-    println!("Example: pymon example.py")
+    println!("Example: pymon example.py");
 }
 
 pub fn run(file_name: &str) {
     if Path::new(file_name).exists() {
         println!("PyMon v2.0");
-        println!("Watching for file changes...")
+        println!("Watching for file changes...");
     } else {
         panic!(
             "[pymon] Error: No files matching the pattern '{}' were found.",
