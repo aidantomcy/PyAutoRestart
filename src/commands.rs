9c98d7d75a5887fb5bad2225643d0821d8f313ea
Aidan Tomcy <aidantomcy@gmail.com>
2022-09-13 10:50:41 +0530

    Add functionality to watch for file changes

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,8 @@
+extern crate notify;
+use notify::{INotifyWatcher, RecursiveMode, Result, Watcher};
+use std::env::consts::OS;
 use std::path::Path;
+use std::process::{Command, ExitStatus};
 
 pub fn help() {
     println!("Usage: pymon <file_name>");
@@ -9,10 +13,43 @@ pub fn run(file_name: &str) {
     if Path::new(file_name).exists() {
         println!("PyMon v2.0");
         println!("Watching for file changes...");
+
+        match OS {
+            "linux" => {
+                let stdout: ExitStatus = Command::new("python3")
+                    .arg(file_name)
+                    .status()
+                    .expect("[pymon] Error: Failed to run file");
+                let output: &str = &stdout.to_string() as &str;
+                println!("{}", output);
+
+                loop {
+                    watch().err();
+                }
+            }
+            "windows" => {
+                todo!("Add functionality for Windows")
+            }
+            _ => panic!("[pymon] Error: Operating System not supported"),
+        }
     } else {
         panic!(
             "[pymon] Error: No files matching the pattern '{}' were found.",
             file_name
         )
     }
+}
+
+fn watch() -> Result<()> {
+    let mut watcher: INotifyWatcher =
+        notify::recommended_watcher(|res: notify::Result<notify::Event>| match res {
+            Ok(_) => {
+                todo!("Implement functionality")
+            }
+            Err(err) => println!("watch error: {:?}", err),
+        })?;
+
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    Ok(())
 }
