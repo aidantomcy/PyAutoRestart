617c28a6b911ef830e23170d7779f130cf840e49
Aidan Tomcy <aidantomcy@gmail.com>
2022-09-18 13:22:27 +0530

    Update code to print colored output

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,10 @@
-extern crate notify;
 use notify::{INotifyWatcher, RecursiveMode, Result, Watcher};
 use std::env::consts::OS;
 use std::env::{args, Args};
+use std::io::{self, Write};
 use std::path::Path;
 use std::process::{Command, ExitStatus};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 pub fn help() {
     println!("Usage: pymon <file_name>");
@@ -12,8 +13,9 @@ pub fn help() {
 
 pub fn run(file_name: &str) {
     if Path::new(file_name).exists() {
-        println!("PyMon v2.0");
-        println!("Watching for file changes...");
+        print_colored_text("success", "PyMon v2.0").err();
+        print_colored_text("warning", "Watching for file changes...").err();
+        reset_stdout_color();
 
         match OS {
             "linux" => {
@@ -45,7 +47,7 @@ fn watch() -> Result<()> {
     let mut watcher: INotifyWatcher =
         notify::recommended_watcher(|res: notify::Result<notify::Event>| match res {
             Ok(_) => {
-                println!("Restarting due to file changes...");
+                print_colored_text("warning", "Restarting due to file changes...").err();
                 let mut args: Args = args();
                 let file_name: &str = &args.nth(1).unwrap() as &str;
                 run(file_name);
@@ -56,4 +58,31 @@ fn watch() -> Result<()> {
     watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
 
     Ok(())
+}
+
+fn print_colored_text(output_type: &str, msg: &str) -> io::Result<()> {
+    let mut stdout: StandardStream = StandardStream::stdout(ColorChoice::Always);
+
+    match output_type {
+        "success" => {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+            writeln!(&mut stdout, "{}", msg)
+        }
+        "warning" => {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+            writeln!(&mut stdout, "{}", msg)
+        }
+        "error" => {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+            writeln!(&mut stdout, "{}", msg)
+        }
+        _ => panic!("Error: Invalid output type provided"),
+    }
+}
+
+fn reset_stdout_color() {
+    let mut stdout: StandardStream = StandardStream::stdout(ColorChoice::Always);
+    stdout
+        .set_color(ColorSpec::new().set_fg(Some(Color::White)))
+        .err();
 }
